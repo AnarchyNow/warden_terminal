f49849ef90d78703a895ee1c0960804b389d91c1
AnarchyNow <88005449+AnarchyNow@users.noreply.github.com>
2021-09-12 15:21:55 -0700

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Command to build on multi platdorm
-docker buildx build --platform linux/arm64,linux/amd64 --tag pxsocs/node_warden:latest --output "type=registry" .
+docker buildx build --platform linux/arm64,linux/amd64 --tag anarchynow/node_warden:latest --output "type=registry" .
